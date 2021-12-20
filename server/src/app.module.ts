606eb3def227c469598a9dae44acfae3aff4b273
Eduardo Vencovsky (Mac M1 2020) <eduvencovsky@gmail.com>
2021-12-20 17:04:37 -0300

    Update workflows

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -13,16 +13,19 @@ import { ProductModule } from './product/product.module';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: configService.get<'postgres'>('DB_TYPE'),
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: configService.get('NODE_ENV') !== 'production',
-      }),
+      useFactory: (configService: ConfigService) => {
+        const a = {
+          type: configService.get<'postgres'>('DB_TYPE'),
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: configService.get('NODE_ENV') !== 'production',
+        };
+        return a;
+      },
     }),
   ],
   controllers: [AppController],
